493ffd7c1af1bf74156b84f5cc6f74b517476d78
emtushar <kaushiktushar6892@gmail.com>
2024-02-02 19:04:02 +0530

    added a new function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const name = String("Tushar");
+const userName = String("Tushar");
 
 const age = Number(23);
+
+const myFunction = function (userName, age) {
+  return `hi ${userName} and ${age}`;
+};
+
+console.log(myFunction(userName, age));
